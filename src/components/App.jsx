94ff516f1706d148d26d9aa42e09e5ae5bf2b3fd
Rishikesh Yadav <77565163+yadavri@users.noreply.github.com>
2022-10-12 14:48:24 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Element from "./Element";
+import emojipedia from "/src/emojipedia";
+
+function CreateElement(element) {
+  return (
+    <Element
+      key={element.id}
+      emoji={element.emoji}
+      name={element.name}
+      meaning={element.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(CreateElement)};</dl>
+    </div>
+  );
+}
+
+export default App;
